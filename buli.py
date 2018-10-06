31f6f6d5a35ff795ad825e2a2dedf0a52486a721
Eric Sommer <ericsomm@gmx.de>
2018-10-06 13:43:41 +0200

    pep8

--- buli.py
+++ buli.py
@@ -2,32 +2,32 @@
 # -*- coding: utf-8 -*-
 
 # Analyze historical Bundesliga results
-# 
+#
 ########################################
 
-#path='/home/eric/Dropbox/buli/'
-path='Z:/test/buli/'
-    
+# path='/home/eric/Dropbox/buli/'
+# path = 'Z:/test/buli/'
+
+
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import os
 
-from buli_rawdata_rssf import *
-from buli_process import *
-from crawler import * 
+# from buli_rawdata_rssf import *
+# from buli_process import *
+from crawler import *
 from colorama import Fore
 from colorama import Style
 
-if not os.path.exists(path):
-    path=os.getcwd()+'/'
+path = os.getcwd() + '/'
 
-#SWITCHES
+# SWITCHES
 
 
 crawl = 0
 
-makefigures = 0
+makefigures = 1
 
 
 # SET VARIABLES FOR OUTPUT
@@ -35,7 +35,7 @@ spieltag = 32
 scf_points = 33
 # Plot since season..
 min_season = 1980
-      
+
 if crawl == 1:
     crawler(path)
 
@@ -47,9 +47,9 @@ if process_data == 1:
     processdata(path)
 """
 
-df = pd.read_pickle(path+'all_kicker_results')
-#drop 2017
-df = df.drop(df[df.season==2017].index)
+df = pd.read_json(path+'all_kicker_results.json')
+# drop 2017
+# df = df.drop(df[df.season==2017].index)
 # drop missing
 df = df.dropna(how='any')
 
@@ -60,116 +60,146 @@ df['awaygoals'] = pd.to_numeric(df['awaygoals'])
 # Baue Teamdatensatz
 dfhome = df.copy()
 dfaway = df.copy()
-        
+
 dfhome['team'] = dfhome['hometeam']
 dfhome['opponent'] = dfhome['awayteam']
 dfhome['goals_for'] = dfhome['homegoals']
 dfhome['goals_against'] = dfhome['awaygoals']
-        
+
 dfaway['team'] = dfaway['awayteam']
 dfaway['opponent'] = dfaway['hometeam']
 dfaway['goals_for'] = dfaway['awaygoals']
 dfaway['goals_against'] = dfaway['homegoals']
-        
-dfhome = dfhome.drop(['hometeam','awayteam','homegoals','awaygoals'],1)
-dfaway = dfaway.drop(['hometeam','awayteam','homegoals','awaygoals'],1)
+
+dfhome = dfhome.drop(['hometeam',
+                      'awayteam',
+                      'homegoals',
+                      'awaygoals'], 1)
+dfaway = dfaway.drop(['hometeam',
+                      'awayteam',
+                      'homegoals',
+                      'awaygoals'], 1)
 dfhome['home'] = 1
 dfaway['home'] = 0
-# Packe home und away zusammen        
+# Packe home und away zusammen
 df = dfhome.append(dfaway)
-#print(pd.crosstab(df['team'], df['season']))    
+# print(pd.crosstab(df['team'], df['season']))
 
 df['team'] = correct_names(df['team'])
 df['opponent'] = correct_names(df['opponent'])
 
-df['pts'] = 3 * (df['goals_for'] > df['goals_against']) + (df['goals_for'] == df['goals_against'])
+df['pts'] = (3 * (df['goals_for'] > df['goals_against']) +
+             (df['goals_for'] == df['goals_against']))
 
-
-df = df.sort_values(by=['season','spieltag'])
-goals_mday = df.groupby(['season','spieltag'])['goals_for'].sum()
+df = df.sort_values(by=['season', 'spieltag'])
+goals_mday = df.groupby(['season', 'spieltag'])['goals_for'].sum()
 goals_mday = goals_mday.sort_values(0)
 mingoals = goals_mday.min()
-#print('Seasons and Matchday with least goals')
-#print(goals_mday[0:9])
-    
+# print('Seasons and Matchday with least goals')
+# print(goals_mday[0:9])
+
 # Now, create Rank.
-df = df.sort_values(by=['season','team','spieltag'])
+df = df.sort_values(by=['season', 'team', 'spieltag'])
 # various cumulative sums
-df['goals_for_cum'] = df.groupby(['season','team'])['goals_for'].apply(lambda x: x.cumsum())
-df['goals_against_cum'] = df.groupby(['season','team'])['goals_against'].apply(lambda x: x.cumsum())
+df['goals_for_cum'] = df.groupby(['season', 'team'])['goals_for'].apply(lambda x: x.cumsum())
+df['goals_against_cum'] = df.groupby(['season', 'team'])['goals_against'].apply(lambda x: x.cumsum())
 df['goal_diff'] = df['goals_for_cum'] - df['goals_against_cum']
-df['points_cum'] = df.groupby(['season','team'])['pts'].apply(lambda x: x.cumsum())
-    
-df = df.sort_values(by=['season','spieltag','points_cum','goal_diff'],ascending=[True,True,False,False])
-df['rank'] = df.groupby(['season','spieltag']).cumcount() + 1
-    
+df['points_cum'] = df.groupby(['season', 'team'])['pts'].apply(lambda x: x.cumsum())
+
+df = df.sort_values(by=['season', 'spieltag', 'points_cum', 'goal_diff'],
+                    ascending=[True, True, False, False])
+df['rank'] = df.groupby(['season', 'spieltag']).cumcount() + 1
+
 # obtain rank at end of season
-end_rank = df[['team','season','rank']][df.spieltag==34]
-end_rank = end_rank.rename(columns={'rank':'end_rank'})
-df = pd.merge(df,end_rank,on=['team','season'])
+end_rank = df[['team', 'season', 'rank']][df.spieltag == 34]
+end_rank = end_rank.rename(columns={'rank': 'end_rank'})
+df = pd.merge(df, end_rank, on=['team', 'season'])
 
 # get difference to each rank
-for p in range(0,18):
-    temp=df[['season','spieltag','points_cum']][df['rank']==p+1]
-    temp = temp.rename(columns={'points_cum':'pts'+str(p+1)})
-    df =pd.merge(df,temp,on=['season','spieltag'])
+for p in range(0, 18):
+    temp = df[['season', 'spieltag', 'points_cum']][df['rank'] == p+1]
+    temp = temp.rename(columns={'points_cum': 'pts' + str(p+1)})
+    df = pd.merge(df, temp, on=['season', 'spieltag'])
     df['diff'+str(p+1)] = df['points_cum'] - df['pts'+str(p+1)]
-    df = df.drop(['pts'+str(p+1)],1)
+    df = df.drop(['pts' + str(p+1)], 1)
 
-#print(df[['season','spieltag','team','rank','points_cum','diff5']].head())
+# print(df[['season','spieltag','team','rank','points_cum','diff5']].head())
+df.to_json(path+'buli_final.json')
 
-pd.to_pickle(df,path+'buli_final')
 
-
-print("Punktzahl aller Zweitplatzierten am ",spieltag,".Spieltag")
-zweite = df[['season','team','points_cum']][(df['spieltag']==spieltag) & (df['rank']==2)]
-#zweite['points_cum'].hist(bins=20)
+print("Punktzahl aller Zweitplatzierten am ", spieltag, ".Spieltag")
+zweite = df[['season', 'team', 'points_cum']][(df['spieltag'] == spieltag) &
+                                              (df['rank'] == 2)]
+# zweite['points_cum'].hist(bins=20)
 zweite = zweite.sort_values(by='points_cum')
 zweite.head(10)
 if makefigures == 1:
-    for sp in range(9,35):
+    for sp in range(9, 35):
         plt.clf()
-        df[df['spieltag']==sp].boxplot(column='points_cum',by=['rank'])
+        df[df['spieltag'] == sp].boxplot(column='points_cum', by=['rank'])
         plt.savefig('box_'+str(sp)+'.png')
 
-print("Endplatzierung aller Mannschaften, die am ",spieltag,".Spieltag ",scf_points, " Punkte hatten.")
-export = df[['season','team','rank','end_rank','diff16','diff17']][(df.spieltag==spieltag)&(df.points_cum==scf_points)]
+print("Endplatzierung aller Mannschaften, die am ",
+      spieltag,
+      ".Spieltag ",
+      scf_points,
+      " Punkte hatten.")
+export = df[['season',
+             'team',
+             'rank',
+             'end_rank',
+             'diff16',
+             'diff17']][(df.spieltag == spieltag) &
+                        (df.points_cum == scf_points)]
 print(export)
 
-
-
-with open(os.path.join(path,'bl_hist_sp'+str(spieltag)+'pt'+str(scf_points)+'.txt'),'w') as outfile:
+with open(os.path.join(path, 'bl_hist_sp' + str(spieltag) +
+                            'pt' + str(scf_points) + '.txt'), 'w') as outfile:
     export.to_string(outfile)
 
 # Noch interessanter: Ziehe eine Liste von Listen, die die Tabellenplatz-Entwicklung dieser Teams beschreibt
-plotcases = df[['season','team','end_rank','diff16','diff17']][(df.spieltag==spieltag)&(df.points_cum==scf_points)&(df.season>=min_season)]
+plotcases = df[['season',
+                'team',
+                'end_rank',
+                'diff16',
+                'diff17']][(df.spieltag == spieltag) &
+                           (df.points_cum == scf_points) &
+                           (df.season >= min_season)]
 plotcases['seasstr'] = plotcases.season.astype(str)
 plotcases['label'] = plotcases['team'] + ' ' + plotcases.seasstr
 
 scfig, ax = plt.subplots()
-ax = plotcases.plot.scatter(x='diff17',y='end_rank')
+ax = plotcases.plot.scatter(x='diff17', y='end_rank')
 ax.invert_yaxis()
 plt.axhline(y=16.5, color='r')
-plotcases[['diff17','end_rank','label']].apply(lambda x: ax.text(*x), axis=1)
+plotcases[['diff17', 'end_rank', 'label']].apply(lambda x: ax.text(*x), axis=1)
 plt.show()
-
-
-sys.exit('DONE')
-ranklist=[]
+'''
+# sys.exit('DONE')
+ranklist = []
 
 for i in plotcases.index:
-    ranklist.append(df['rank'][(df['season']==plotcases['season'][i])&(df['team']==plotcases['team'][i])&(df['spieltag']>=spieltag)]& (df['season'] >= min_season))
+    ranklist.append(df['rank'][(df['season'] == plotcases['season'][i]) &
+                               (df['team'] == plotcases['team'][i]) &
+                               (df['spieltag'] >= spieltag) &
+                               (df['season'] >= min_season)
+                               ])
 plotcases['ranklist'] = ranklist
 
 
-
-#spieltagplot = np.arange(spieltag,35)
+spieltagplot = np.arange(spieltag,35)
 # Now plot this
-#plt.clf()
-#for i in plotcases.index:
- #   print(i)    
-  #  plt.plot(spieltagplot,plotcases['ranklist'][i],label=plotcases['team'][i]+' '+str(plotcases['season'][i]))
+plt.clf()
+for i in plotcases.index:
+    print(i)
+    plt.plot(spieltagplot,
+             plotcases['ranklist'][i],
+             label=plotcases['team'][i]+' '+str(plotcases['season'][i]))
 
-#plt.legend(loc='upper center', bbox_to_anchor=(spieltagplot[0], -0.05),fancybox=True, shadow=True, ncol=5)
-#plt.show()
-
+plt.legend(loc='upper center',
+           bbox_to_anchor=(spieltagplot[0], -0.05),
+           fancybox=True,
+           shadow=True,
+           ncol=5)
+plt.show()
+'''
--- crawler.py
+++ crawler.py
@@ -1,20 +1,20 @@
 # -*- coding: utf-8 -*-
 """
-CRAWLING FUSSBALLDATEN.DE
+CRAWLING KICKER.DE
 """
-import urllib.request as MyBrowser 
+import urllib.request as MyBrowser
 import os
 import re
 import pandas as pd
 import numpy as np
 
-seasons = np.arange(1963,2018)
+seasons = np.arange(1963, 2019)
 
-def correct_names(str_list):        
+def correct_names(str_list):
     res = list(str_list)
-    tmp_lst = list(str_list)        
-    #for s,t in str_list.iteritems():                       
-    for s in range(0,len(str_list)):            
+    tmp_lst = list(str_list)
+    # for s,t in str_list.iteritems():                       
+    for s in range(0, len(str_list)):
         if tmp_lst[s] == 'Meidericher SV':
             res[s] = 'Duisburg'
         if tmp_lst[s] == 'Waldhof':
@@ -24,43 +24,52 @@ def correct_names(str_list):
     return res
 
 
-
-def mkURL(season,spieltag):
-    seasonstring = str(season)+'-'+str(season+1)[-2:]
-    url = "http://www.kicker.de/news/fussball/bundesliga/spieltag/1-bundesliga/"+seasonstring+'/'+str(spieltag)+'/0/spieltag.html'
+def mkURL(season, spieltag):
+    seasonstring = str(season) + '-' + str(season+1)[-2:]
+    url = ("http://www.kicker.de/news/fussball/bundesliga/spieltag/1-bundesliga/" +
+           seasonstring +
+           '/' + str(spieltag) + '/0/spieltag.html')
     return url
-    
+
+
 def crawler(path):
-    datadir=path+'data/'    
+    datadir = path + 'data/'
     if not os.path.exists(datadir):
         os.mkdir(datadir)
-        
-    rawdir=datadir+'raw/'
+
+    rawdir = datadir + 'raw/'
     if not os.path.exists(rawdir):
         os.mkdir(rawdir)
-        
-    
+
     for s in seasons:
         print('Downloading season ' + str(s) + '...')
         for sp in range(1,35):
-            request = MyBrowser.Request((mkURL(s,sp)))
-            
-            rawfile = rawdir+'kicker_'+str(s)+'_'+str(sp)+".html"
-            if not os.path.isfile(rawfile):                
-                response = MyBrowser.urlopen(request)                   
-                page = response.read()                                 
-                file = open(rawfile, "wb")              
+            request = MyBrowser.Request((mkURL(s, sp)))
+
+            rawfile = rawdir + 'kicker_' + str(s) + '_' + str(sp) + ".html"
+            if not os.path.isfile(rawfile):
+                response = MyBrowser.urlopen(request)
+                page = response.read()
+                file = open(rawfile, "wb")
                 file.write(page)
                 file.close()
-                
+
     print("Finished Downloading")
 
-    buli_results = pd.DataFrame(columns=['season','spieltag','hometeam','awayteam','homegoals','awaygoals'])    
+    buli_results = pd.DataFrame(columns=['season',
+                                         'spieltag',
+                                         'hometeam',
+                                         'awayteam',
+                                         'homegoals',
+                                         'awaygoals'])
     for s in seasons:
-        print(str(s),end=' ')
-        for sp in range(1,35):
-            
-            html = open(rawdir+'/kicker_'+str(s)+'_'+str(sp)+".html","r",encoding="utf-8").read()
+        print(str(s), end=' ')
+        for sp in range(1, 35):
+            html = open(rawdir +
+                        '/kicker_' +
+                        str(s) + '_' + str(sp) + ".html",
+                        "r",
+                        encoding="utf-8").read()
             # find hometeam, awayteam, result in string.
             hometeam = []
             awayteam = []
@@ -69,8 +78,7 @@ def crawler(path):
             HomeRegEx = re.compile('class="ovVrn ovVrnRight">(.+?)</a>')
             AwayRegEx = re.compile('class="ovVrn">(.+?)</a>')
             HomeGoalsRegEx = re.compile('<td class="alignleft nowrap" >(\d*):')
-            AwayGoalsRegEx = re.compile('<td class="alignleft nowrap" >\d*:(\d*)&nbsp;')
-            
+            AwayGoalsRegEx = re.compile('<td class="alignleft nowrap" >\d*:(\d*)&nbsp;')            
 
             for match in HomeRegEx.finditer(html):
                 hometeam.append(match.group(1))
@@ -80,15 +88,22 @@ def crawler(path):
                 homegoals.append(match.group(1))
             for match in AwayGoalsRegEx.finditer(html):
                 awaygoals.append(match.group(1))
-            
-            spt=pd.DataFrame(data=[[s]*9,[sp]*9,hometeam,awayteam,homegoals,awaygoals]).T      
-            spt = spt.rename(columns={0:'season',1:'spieltag',2:'hometeam',3:'awayteam',4:'homegoals',5:'awaygoals'})
-            buli_results = buli_results.append(spt,ignore_index=True)
-            
+
+            spt = pd.DataFrame(data=[[s]*9,
+                                     [sp]*9,
+                                     hometeam,
+                                     awayteam,
+                                     homegoals,
+                                     awaygoals]).T
+            spt = spt.rename(columns={0: 'season',
+                                      1: 'spieltag',
+                                      2: 'hometeam',
+                                      3: 'awayteam',
+                                      4: 'homegoals',
+                                      5: 'awaygoals'})
+            buli_results = buli_results.append(spt, ignore_index=True)
+
     # save raw data        
-    buli_results.to_pickle(path+'all_kicker_results')
+    buli_results.to_json(path + 'all_kicker_results.json')
 
-    return 0
-
-
-    
+    return True    
