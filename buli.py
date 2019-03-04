5ed4cbd1cbbd3c9e94bb6f16d5e3602a3afbad56
Eric Sommer <ericspam@mail.de>
2019-03-04 22:12:42 +0100

    Collecting goals works now

--- buli.py
+++ buli.py
@@ -10,10 +10,8 @@
 
 
 import pandas as pd
-import numpy as np
 import matplotlib.pyplot as plt
 import os
-import sys
 from functions import get_full_team_names
 
 # from buli_rawdata_rssf import *
@@ -24,222 +22,263 @@ path = os.getcwd() + "/"
 
 # SWITCHES
 
-# Crawl and reproduce data (to make sure ID's are OK?)
+# Crawl and reproduce data.
 crawl = 1
 
 produce_graphs = 0
 
+teamname = "Freiburg"
+
+seasons_to_crawl = list(range(1995,2017))
+
 # SET VARIABLES FOR OUTPUT
-spieltag = 22
-scf_points = 24
+spieltag = 23
+team_points = 16
 # Plot since season..
 min_season = 1980
 
-seasons_to_crawl = [2017]
-if crawl == 1:
-    df = crawler(path, seasons_to_crawl)
-else:
-    df = pd.read_json(path + "all_kicker_results.json")
-# drop 2017
-# drop missing
-df = df.dropna(how="any")
-df["season"] = df["season"].astype(int)
-# few checks on the data
-print(df.sort_values(by=["season"])["season"].value_counts())
-
-df["hometeam"] = df["hometeam"].astype(str)
-df["awayteam"] = df["awayteam"].astype(str)
-df["homegoals"] = pd.to_numeric(df["homegoals"])
-df["awaygoals"] = pd.to_numeric(df["awaygoals"])
-# Baue Teamdatensatz
-dfhome = df.copy()
-dfaway = df.copy()
-
-dfhome["team"] = dfhome["hometeam"]
-dfhome["opponent"] = dfhome["awayteam"]
-dfhome["goals_for"] = dfhome["homegoals"]
-dfhome["goals_against"] = dfhome["awaygoals"]
-
-dfaway["team"] = dfaway["awayteam"]
-dfaway["opponent"] = dfaway["hometeam"]
-dfaway["goals_for"] = dfaway["awaygoals"]
-dfaway["goals_against"] = dfaway["homegoals"]
-
-dfhome = dfhome.drop(["hometeam", "awayteam", "homegoals", "awaygoals"], 1)
-dfaway = dfaway.drop(["hometeam", "awayteam", "homegoals", "awaygoals"], 1)
-dfhome["home"] = 1
-dfaway["home"] = 0
-# Packe home und away zusammen
-df = dfhome.append(dfaway)
-
-df["team"] = correct_names(df["team"])
-df["opponent"] = correct_names(df["opponent"])
-# Correct team names
-for t in ["team", "opponent"]:
-    df.loc[(df[t] == "Leipzig") & (df["season"] == 1993), t] = "Leipzig2"
-
-df["pts"] = 3 * (df["goals_for"] > df["goals_against"]) + (
-    df["goals_for"] == df["goals_against"]
-)
-
-
-df = df.sort_values(by=["season", "spieltag"])
-goals_mday = df.groupby(["season", "spieltag"])["goals_for"].sum()
-goals_mday = goals_mday.sort_values(0)
-mingoals = goals_mday.min()
-
-# Now, create Rank.
-df = df.sort_values(by=["season", "team", "spieltag"])
-# various cumulative sums
-df["goals_for_cum"] = df.groupby(["season", "team"])["goals_for"].apply(
-    lambda x: x.cumsum()
-)
-df["goals_against_cum"] = df.groupby(["season", "team"])["goals_against"].apply(
-    lambda x: x.cumsum()
-)
-df["goal_diff"] = df["goals_for_cum"] - df["goals_against_cum"]
-df["points_cum"] = df.groupby(["season", "team"])["pts"].apply(lambda x: x.cumsum())
-
-df = df.sort_values(
-    by=["season", "spieltag", "points_cum", "goal_diff"],
-    ascending=[True, True, False, False],
-)
-df["rank"] = df.groupby(["season", "spieltag"]).cumcount() + 1
-
-# obtain rank at end of season
-end_rank = df[["team", "season", "rank"]][df.spieltag == 34]
-end_rank = end_rank.rename(columns={"rank": "end_rank"})
-df = pd.merge(df, end_rank, on=["team", "season"])
-
-# get difference to each rank
-for p in range(0, 18):
-    temp = df[["season", "spieltag", "points_cum"]][df["rank"] == p + 1]
-    temp = temp.rename(columns={"points_cum": "pts" + str(p + 1)})
-    df = pd.merge(df, temp, on=["season", "spieltag"])
-    df["diff" + str(p + 1)] = df["points_cum"] - df["pts" + str(p + 1)]
-    df = df.drop(["pts" + str(p + 1)], 1)
-
-# print(df[['season','spieltag','team','rank','points_cum','diff5']].head())
-# pd.to_pickle(df, path + "buli_final")
-
-# print("Punktzahl aller Zweitplatzierten am ",spieltag,".Spieltag")
-# zweite = df[['season','team','points_cum']][(df['spieltag']==spieltag) & (df['rank']==2)]
-# zweite['points_cum'].hist(bins=20)
-# zweite = zweite.sort_values(by='points_cum')
-# zweite.head(10)
-if produce_graphs == 1:
+def make_boxplot_by_spieltag(df):
     for sp in range(9, 35):
         plt.clf()
         df[df["spieltag"] == sp].boxplot(column="points_cum", by=["rank"])
         plt.savefig("box_" + str(sp) + ".png")
         plt.close()
-"""
-print("Endplatzierung aller Mannschaften, die am ",
-      spieltag,
-      ".Spieltag ",
-      scf_points, " Punkte hatten.")
-"""
-export = df[["season", "team", "rank", "end_rank", "diff16", "diff17"]][
-    (df.spieltag == spieltag) & (df.points_cum == scf_points)
-]
 
-with open(
-    os.path.join(path, "bl_hist_sp" + str(spieltag) + "pt" + str(scf_points) + ".txt"),
-    "w",
-) as outfile:
-    export.to_string(outfile)
-
-# Suche längste Serie ohne Tor
-df = df.sort_values(by=["season", "team", "spieltag"])
-df["5gamesnogoal"] = (
-    (df["goals_for"] == 0)
-    & (df["goals_for"].shift(1) == 0)
-    & (df["goals_for"].shift(2) == 0)
-    & (df["goals_for"].shift(3) == 0)
-    & (df["goals_for"].shift(4) == 0)
-)
-
-# Noch interessanter: Ziehe eine Liste von Listen, die die Tabellenplatz-Entwicklung dieser Teams beschreibt
-plotcases = df[["season", "team", "end_rank", "diff16", "diff17"]][
-    (df.spieltag == spieltag)
-    & (df.points_cum == scf_points)
-    & (df.season >= min_season)
-]
-plotcases["seasstr"] = plotcases.season.astype(str)
-plotcases["label"] = plotcases["team"] + " " + plotcases.seasstr
-
-scfig, ax = plt.subplots()
-ax = plotcases.plot.scatter(x="diff17", y="end_rank")
-ax.invert_yaxis()
-plt.axhline(y=16.5, color="r")
-plotcases[["diff17", "end_rank", "label"]].apply(lambda x: ax.text(*x), axis=1)
-plt.show()
-
-for var in ["season", "spieltag", "pts", "goals_for", "goals_against", "rank"]:
-    df[var] = df[var].astype(int)
-
-# Ewige Tabelle
-# Now, create Rank.
-df = df.sort_values(by=["season", "team", "spieltag"])
-# various cumulative sums
-df["goals_for_cum_ever"] = df.groupby(["team"])["goals_for"].apply(lambda x: x.cumsum())
-df["goals_against_cum_ever"] = df.groupby(["team"])["goals_against"].apply(
-    lambda x: x.cumsum()
-)
-df["goal_diff_ever"] = df["goals_for_cum_ever"] - df["goals_against_cum_ever"]
-df["points_cum_ever"] = df.groupby(["team"])["pts"].apply(lambda x: x.cumsum())
-# Number of seasons played. don't know yet how to bring to ewige tabelle
-n_seasons = df.groupby("team")["season"].unique().apply(len)
-# n_seasons['team'] = n_seasons.index
-# also add number of games, including wins, draws, losses
-
-ewigetabelle = df.groupby("team").last()
-ewigetabelle["team"] = ewigetabelle.index
-ewigetabelle = ewigetabelle.sort_values(
-    by=["points_cum_ever", "goal_diff_ever"], ascending=[False, False]
-)
-ewigetabelle["rank"] = ewigetabelle.rank(ascending=True)
-
-full_names = get_full_team_names()
-
-ewigetabelle = ewigetabelle.replace({"team": full_names})
-
-ewigetabelle[
-    [
-        "rank",
-        "team",
-        "goals_for_cum_ever",
-        "goals_against_cum_ever",
-        "goal_diff_ever",
-        "points_cum_ever",
+def get_prob_abstieg(df, spieltag, team_points):
+    """ A team has 'team_points' after matchday 'spieltag'
+        How did other teams with these parameters fare in the past regarding relegation?
+    """
+    export = df[["season", "team", "rank", "end_rank", "diff16", "diff17"]][
+        (df.spieltag == spieltag) & (df.points_cum == team_points)
     ]
-].to_excel("ewigetabelle.xls")
 
-# Ab wann ist die Tabelle aussagekräftig
-df["diff_rank"] = df["end_rank"] - df["rank"]
-df["close"] = abs(df["diff_rank"]) <= 2
-print(df.groupby(["spieltag"])["close"].mean())
+    with open(
+        os.path.join(path, "bl_hist_sp" + str(spieltag) + "pt" + str(team_points) + ".txt"),
+        "w",
+    ) as outfile:
+        export.to_string(outfile)
 
-# Bilanz SC Freiburg nach Gegner
-scf = df[df["team"] == "Freiburg"]
-scf["win"] = 1 * (scf["goals_for"] > scf["goals_against"])
-scf["draw"] = 1 * (scf["goals_for"] == scf["goals_against"])
-scf["loss"] = 1 * (scf["goals_for"] < scf["goals_against"])
-scfbilanz = scf.groupby("opponent")[
-    ["pts", "goals_for", "goals_against", "win", "draw", "loss"]
-].sum()
-scfbilanz["goal_diff"] = scfbilanz["goals_for"] - scfbilanz["goals_against"]
-scfbilanz = scfbilanz.sort_values(by=["pts", "goal_diff"], ascending=[False, False])
-print("SC Bilanz")
-print(scfbilanz[["pts", "goal_diff", "win", "draw", "loss"]])
-print("SC Spiele mit mehr als 5 Toren")
-print(
-    df[["season", "spieltag", "team", "opponent", "goals_for", "goals_against"]][
-        (df["team"] == "Freiburg") & (df["goals_for"] >= 5)
+    # Noch interessanter: Ziehe eine Liste von Listen, die die Tabellenplatz-Entwicklung dieser Teams beschreibt
+    plotcases = df[["season", "team", "end_rank", "diff16", "diff17"]][
+        (df.spieltag == spieltag)
+        & (df.points_cum == team_points)
+        & (df.season >= min_season)
     ]
-)
+    plotcases["seasstr"] = plotcases.season.astype(str)
+    plotcases["label"] = plotcases["team"] + " " + plotcases.seasstr
 
-sys.exit("DONE")
+    scfig, ax = plt.subplots()
+    ax = plotcases.plot.scatter(x="diff17", y="end_rank")
+    ax.invert_yaxis()
+    plt.axhline(y=16.5, color="r")
+    plotcases[["diff17", "end_rank", "label"]].apply(lambda x: ax.text(*x), axis=1)
+    plt.show()
+
+def get_streaks(df):
+    """ Looking for a goalless streak
+    """
+    df = df.sort_values(by=["season", "team", "spieltag"])
+    df["5gamesnogoal"] = (
+        (df["goals_for"] == 0)
+        & (df["goals_for"].shift(1) == 0)
+        & (df["goals_for"].shift(2) == 0)
+        & (df["goals_for"].shift(3) == 0)
+        & (df["goals_for"].shift(4) == 0)
+    )
+
+
+def ewigetabelle(df):
+    """ Ewige Tabelle: Cumulated Table of all seasons
+    """
+    # Now, create Rank.
+    df = df.sort_values(by=["season", "team", "spieltag"])
+    # various cumulative sums
+    df["goals_for_cum_ever"] = df.groupby(["team"])["goals_for"].apply(lambda x: x.cumsum())
+    df["goals_against_cum_ever"] = df.groupby(["team"])["goals_against"].apply(
+        lambda x: x.cumsum()
+    )
+    df["goal_diff_ever"] = df["goals_for_cum_ever"] - df["goals_against_cum_ever"]
+    df["points_cum_ever"] = df.groupby(["team"])["pts"].apply(lambda x: x.cumsum())
+    # Number of seasons played. don't know yet how to bring to ewige tabelle
+    n_seasons = df.groupby("team")["season"].unique().apply(len)
+    # n_seasons['team'] = n_seasons.index
+    # also add number of games, including wins, draws, losses
+
+    ewigetabelle = df.groupby("team").last()
+    ewigetabelle["team"] = ewigetabelle.index
+    ewigetabelle = ewigetabelle.sort_values(
+        by=["points_cum_ever", "goal_diff_ever"], ascending=[False, False]
+    )
+    ewigetabelle["rank"] = ewigetabelle.rank(ascending=True)
+
+    full_names = get_full_team_names()
+
+    ewigetabelle = ewigetabelle.replace({"team": full_names})
+
+    ewigetabelle[
+        [
+            "rank",
+            "team",
+            "goals_for_cum_ever",
+            "goals_against_cum_ever",
+            "goal_diff_ever",
+            "points_cum_ever",
+        ]
+    ].to_excel("ewigetabelle.xls")
+
+def teambilanz(df, teamname="Freiburg"):
+    """ Show historical matchup of a team against all other Teams.
+    """
+    # keep only team
+    teamdf = df[df["team"] == teamname]
+    teamdf["win"] = 1 * (teamdf["goals_for"] > teamdf["goals_against"])
+    teamdf["draw"] = 1 * (teamdf["goals_for"] == teamdf["goals_against"])
+    teamdf["loss"] = 1 * (teamdf["goals_for"] < teamdf["goals_against"])
+    bilanz = teamdf.groupby("opponent")[
+        ["pts", "goals_for", "goals_against", "win", "draw", "loss"]
+    ].sum()
+    bilanz["goal_diff"] = bilanz["goals_for"] - bilanz["goals_against"]
+    bilanz = bilanz.sort_values(by=["pts", "goal_diff"], ascending=[False, False])
+    print("SC Bilanz")
+    print(bilanz[["pts", "goal_diff", "win", "draw", "loss"]])
+    print("Spiele von {} mit mehr als 5 Toren".format(teamname))
+    print(
+        df[["season", "spieltag", "team", "opponent", "goals_for", "goals_against"]][
+            (df["team"] == teamname) & (df["goals_for"] >= 5)
+        ]
+    )
+
+
+def game_analysis(df):
+    # drop missing
+    df = df.dropna(how="any")
+    df["season"] = df["season"].astype(int)
+    # few checks on the data
+    print(df.sort_values(by=["season"])["season"].value_counts())
+
+    df["hometeam"] = df["hometeam"].astype(str)
+    df["awayteam"] = df["awayteam"].astype(str)
+    df["homegoals"] = pd.to_numeric(df["homegoals"])
+    df["awaygoals"] = pd.to_numeric(df["awaygoals"])
+    # Baue Teamdatensatz
+    dfhome = df.copy()
+    dfaway = df.copy()
+
+    dfhome["team"] = dfhome["hometeam"]
+    dfhome["opponent"] = dfhome["awayteam"]
+    dfhome["goals_for"] = dfhome["homegoals"]
+    dfhome["goals_against"] = dfhome["awaygoals"]
+
+    dfaway["team"] = dfaway["awayteam"]
+    dfaway["opponent"] = dfaway["hometeam"]
+    dfaway["goals_for"] = dfaway["awaygoals"]
+    dfaway["goals_against"] = dfaway["homegoals"]
+
+    dfhome = dfhome.drop(["hometeam", "awayteam", "homegoals", "awaygoals"], 1)
+    dfaway = dfaway.drop(["hometeam", "awayteam", "homegoals", "awaygoals"], 1)
+    dfhome["home"] = 1
+    dfaway["home"] = 0
+    # Packe home und away zusammen
+    df = dfhome.append(dfaway)
+
+    df["team"] = correct_names(df["team"])
+    df["opponent"] = correct_names(df["opponent"])
+    # Correct team names
+    for t in ["team", "opponent"]:
+        df.loc[(df[t] == "Leipzig") & (df["season"] == 1993), t] = "Leipzig2"
+
+    df["pts"] = 3 * (df["goals_for"] > df["goals_against"]) + (
+        df["goals_for"] == df["goals_against"]
+    )
+
+    for var in ["season", "spieltag", "pts", "goals_for", "goals_against"]:
+        df[var] = df[var].astype(int)
+
+
+    df = df.sort_values(by=["season", "spieltag"])
+    goals_mday = df.groupby(["season", "spieltag"])["goals_for"].sum()
+    goals_mday = goals_mday.sort_values(0)
+    # mingoals = goals_mday.min()
+
+    # Now, create Rank.
+    df = df.sort_values(by=["season", "team", "spieltag"])
+    # various cumulative sums
+    df["goals_for_cum"] = df.groupby(["season", "team"])["goals_for"].apply(
+        lambda x: x.cumsum()
+    )
+    df["goals_against_cum"] = df.groupby(["season", "team"])["goals_against"].apply(
+        lambda x: x.cumsum()
+    )
+    df["goal_diff"] = df["goals_for_cum"] - df["goals_against_cum"]
+    df["points_cum"] = df.groupby(["season", "team"])["pts"].apply(lambda x: x.cumsum())
+
+    df = df.sort_values(
+        by=["season", "spieltag", "points_cum", "goal_diff"],
+        ascending=[True, True, False, False],
+    )
+    df["rank"] = df.groupby(["season", "spieltag"]).cumcount() + 1
+
+    # obtain rank at end of season
+    end_rank = df[["team", "season", "rank"]][df.spieltag == 34]
+    end_rank = end_rank.rename(columns={"rank": "end_rank"})
+    df = pd.merge(df, end_rank, on=["team", "season"])
+
+    # get difference to each rank
+    for p in range(0, 18):
+        temp = df[["season", "spieltag", "points_cum"]][df["rank"] == p + 1]
+        temp = temp.rename(columns={"points_cum": "pts" + str(p + 1)})
+        df = pd.merge(df, temp, on=["season", "spieltag"])
+        df["diff" + str(p + 1)] = df["points_cum"] - df["pts" + str(p + 1)]
+        df = df.drop(["pts" + str(p + 1)], 1)
+
+    # print(df[['season','spieltag','team','rank','points_cum','diff5']].head())
+    # pd.to_pickle(df, path + "buli_final")
+
+    # print("Punktzahl aller Zweitplatzierten am ",spieltag,".Spieltag")
+    # zweite = df[['season','team','points_cum']][(df['spieltag']==spieltag) & (df['rank']==2)]
+    # zweite['points_cum'].hist(bins=20)
+    # zweite = zweite.sort_values(by='points_cum')
+    # zweite.head(10)
+    if produce_graphs == 1:
+        make_boxplot_by_spieltag(df)
+
+    get_prob_abstieg(df, spieltag, team_points)
+
+    get_streaks(df)
+
+    ewigetabelle(df)
+
+    # Ab wann ist die Tabelle aussagekräftig
+    df["diff_rank"] = df["end_rank"] - df["rank"]
+    df["close"] = abs(df["diff_rank"]) <= 2
+    print(df.groupby(["spieltag"])["close"].mean())
+
+
+    teambilanz(df, teamname)
+
+def goal_analysis(df):
+
+    # Torschützenliste
+    print(df['game_id'][~df['owngoal']].groupby(df['scorer']).count().sort_values(ascending=False))
+
+    # what do I want now?
+
+
+
+
+
+
+# Load Data
+if crawl == 1:
+    gameresults, goals = crawler(path, seasons_to_crawl)
+else:
+    gameresults = pd.read_json("all_game_results_since{}.json".format(seasons_to_crawl[0]))
+    goals = pd.read_json('all_goals_since{}.json'.format(seasons_to_crawl[0]))
+
+game_analysis(gameresults)
+
+
+goal_analysis(goals)
 
 """
 ranklist=[]
--- crawler.py
+++ crawler.py
@@ -35,7 +35,12 @@ def dl_and_save(targetfile, request):
     """ carries out a HTML request and saves the response to HD
     """
     print("Writing {}".format(targetfile))
-    response = MyBrowser.urlopen(request)
+    try:
+        response = MyBrowser.urlopen(request)
+    except:
+        print("Request {} not possible".format(request))
+        return ""
+
     page = response.read()
     file = open(targetfile, "wb")
     file.write(page)
@@ -44,6 +49,10 @@ def dl_and_save(targetfile, request):
     return page
 
 def crawler(path, seasons):
+    ''' Crawls through the seasons
+        If file is not there, it is downloaded.
+    '''
+
     datadir = path + "data/"
     if not os.path.exists(datadir):
         os.mkdir(datadir)
@@ -63,9 +72,9 @@ def crawler(path, seasons):
 
     print("Finished Downloading")
 
-    game_results = get_game_results(seasons, rawdir, path)
+    game_results, goals = get_game_results(seasons, rawdir, path)
 
-    return game_results
+    return game_results, goals
 
 
 def get_game_results(seasons, rawdir, path):
@@ -83,6 +92,13 @@ def get_game_results(seasons, rawdir, path):
                  "gamelink"
                  ]
     )
+    HomeRegEx = re.compile('class="ovVrn ovVrnRight">(.+?)</a>')
+    AwayRegEx = re.compile('class="ovVrn">(.+?)</a>')
+    HomeGoalsRegEx = re.compile(r'<td class="alignleft nowrap" >(\d*):')
+    AwayGoalsRegEx = re.compile(
+            r'<td class="alignleft nowrap" >\d*:(\d*)&nbsp;'
+    )
+
     for s in seasons:
         print(str(s), end=" ")
         for sp in range(1, 35):
@@ -97,12 +113,7 @@ def get_game_results(seasons, rawdir, path):
             homegoals = []
             awaygoals = []
             gamelink = []
-            HomeRegEx = re.compile('class="ovVrn ovVrnRight">(.+?)</a>')
-            AwayRegEx = re.compile('class="ovVrn">(.+?)</a>')
-            HomeGoalsRegEx = re.compile(r'<td class="alignleft nowrap" >(\d*):')
-            AwayGoalsRegEx = re.compile(
-                r'<td class="alignleft nowrap" >\d*:(\d*)&nbsp;'
-            )
+
             GameLinkRegEx = re.compile('class="link" href="(.+?)">Analyse')
 
             for match in HomeRegEx.finditer(html):
@@ -133,40 +144,52 @@ def get_game_results(seasons, rawdir, path):
             buli_results = buli_results.append(spt, ignore_index=True)
 
     buli_results['game_id'] = buli_results.index
+    buli_results = buli_results[buli_results['gamelink'] != ""]
+
     # initiate goals data frame
     goals = pd.DataFrame(columns = ['game_id',
                                     'goal_nr',
                                     'scorer',
                                     'minute'
                                     ])
-    # Download game details
-    print(buli_results[['gamelink', 'game_id']].head())
+    # Download game details.
+
     for g, gid in zip(buli_results['gamelink'], buli_results['game_id']):
-        gamefile = "{}game_{}.html".format('data/games/',
-                    gid)
-        request = MyBrowser.Request('http://www.kicker.de{}'.format(g))
-        if not os.path.exists(gamefile):
-            dl_and_save(gamefile, request)
-        # Now
-        html = open('data/games/game_{}.html'.format(gid),
-                "r",
-                encoding="utf-8",
-            ).read()
-        goals_one_g = get_game_details(html, gid)
-        goals_one_g['game_id'] = gid
+        if g != "":
+            gamefile = "{}game_{}.html".format('data/games/',
+                        gid)
 
-        goals = goals.append(goals_one_g, ignore_index=True)
+            request = MyBrowser.Request('http://www.kicker.de{}'.format(g))
+            if not os.path.exists(gamefile):
+                html = dl_and_save(gamefile, request)
+            else:
+                html = open('data/games/game_{}.html'.format(gid),
+                        "r",
+                        encoding="utf-8",
+                    ).read()
+            if html != "":
+                if (gid % 50) == 0:
+                    print("Getting goals from Matchday {} Season {}".format(
+                            buli_results['spieltag'][buli_results['game_id'] == gid].max(),
+                            buli_results['season'][buli_results['game_id'] == gid].max()
+                            )
+                    )
+                goals_one_g = get_game_details(html, gid)
+                goals_one_g['game_id'] = gid
 
+                goals = goals.append(goals_one_g, ignore_index=True)
     # save raw data
-    buli_results.to_json(path + "all_game_results.json")
-    goals.to_json(path + "all_goals.json")
+    buli_results.to_json(path + "all_game_results_since{}.json".format(seasons[0]))
+    goals.to_json(path + "all_goals_since{}.json".format(seasons[0]))
 
-    print(goals)
-
-    return buli_results
+    return buli_results, goals
 
 
 def get_game_details(html, game_id):
+    """ returns a dataframe with the following items from the particular game.
+
+
+    """
     gnr = 0
     out = pd.DataFrame(columns = ['goal_nr',
                                   'scorer',
@@ -176,6 +199,7 @@ def get_game_details(html, game_id):
     goal_nr = []
     scorer = []
     minute = []
+    eg = []
     # loop through all goals
     for bit in game.find_all("div", {"class": "spieler"}):
         gnr += 1
@@ -183,10 +207,21 @@ def get_game_details(html, game_id):
         # the first entry is the scorer, the second one the assist (if available)
         scorer.append(re.findall(ScorerRegEx, str(bit))[0])
         minute.append(int(re.findall(r'\((\d+)\.', str(bit))[0]))
+        try:
+            egstring = re.findall(r'\(\d+\., (.+?),', str(bit))[0]
+            if egstring == "Eigentor":
+                eg.append(True)
+            else:
+                eg.append(False)
+        except IndexError:
+            eg.append(False)
+
+
 
     out = out.append(pd.DataFrame({'goal_nr': goal_nr,
                                    'scorer': scorer,
-                                   'minute': minute},
+                                   'minute': minute,
+                                   'owngoal': eg},
         ), ignore_index=True)
 
     return out
